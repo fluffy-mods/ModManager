83e01965c192f4cc2512f3e706a6c6be8a197cbb
Fluffy <fluffy.l2032@gmail.com>
2018-10-02 10:42:46 +0200

    'Release 0.11.544 [nolog]'

--- Source/ModManager/Properties/AssemblyInfo.cs
+++ Source/ModManager/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.10.543")]
-[assembly: AssemblyFileVersion("0.10.543")]
+[assembly: AssemblyVersion("0.11.544")]
+[assembly: AssemblyFileVersion("0.11.544")]
