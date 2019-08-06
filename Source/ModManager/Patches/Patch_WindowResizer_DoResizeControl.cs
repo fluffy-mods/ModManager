c75a45bf9f4b706c84059a5eb9dcf858ae1ceb45
Fluffy <fluffy.l2032@gmail.com>
2019-08-06 22:27:04 +0200

    cancel resizer on mouseUp outside of game window and game focus loss
    should fix anoter of the issues in #49

---
+++ Source/ModManager/Patches/Patch_WindowResizer_DoResizeControl.cs
@@ -0,0 +1,19 @@
+﻿// Patch_WindowResizer_DoResizeControl.cs
+// Copyright Karel Kroeze, 2019-2019
+
+using Harmony;
+using UnityEngine;
+using Verse;
+
+namespace ModManager
+{
+    [HarmonyPatch(typeof(WindowResizer), nameof(WindowResizer.DoResizeControl))]
+    public static class Patch_WindowResizer_DoResizeControl
+    {
+        public static void PostFix( ref bool ___isResizing )
+        {
+            if ( ___isResizing && ( !Input.GetMouseButton( 0 ) || !Application.isFocused ) )
+                ___isResizing = false;
+        }
+    }
+}
