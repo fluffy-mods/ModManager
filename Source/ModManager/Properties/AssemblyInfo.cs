f830f95daaac5149b39be1ea4ac0e6324d448517
Fluffy <fluffy.l2032@gmail.com>
2019-08-09 22:38:46 +0200

    'Release 1.31.871 [nolog]'

--- Source/ModManager/Properties/AssemblyInfo.cs
+++ Source/ModManager/Properties/AssemblyInfo.cs
@@ -32,4 +32,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.30.870")]
+[assembly: AssemblyFileVersion("1.31.871")]
