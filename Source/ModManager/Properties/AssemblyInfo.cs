c405431e6a8e40d11ade5b23dbb20ff8721a8777
Fluffy <fluffy.l2032@gmail.com>
2020-08-30 11:27:06 +0200

    Release 3.7.1004 [nolog]

--- Source/ModManager/Properties/AssemblyInfo.cs
+++ Source/ModManager/Properties/AssemblyInfo.cs
@@ -32,4 +32,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("3.0.0")]
-[assembly: AssemblyFileVersion("3.6.1003")]
+[assembly: AssemblyFileVersion("3.7.1004")]
